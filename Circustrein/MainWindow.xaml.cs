43a86d98a06509a4b043bce2cc246f5eb68dc60e
Robbert Lengton <robbert.lengton@hotmail.nl>
2020-10-09 16:04:04 +0200

    Kleine aanpassingen

--- Circustrein/MainWindow.xaml.cs
+++ Circustrein/MainWindow.xaml.cs
@@ -29,17 +29,14 @@ namespace Circustrein
             List<Animal> animals = new List<Animal>();
             foreach (TextBox tbx in FindVisualChildren<TextBox>(dpAnimalInput))
             {
-                if (tbx.Text != "0")
-                {
-                    string[] arrTbxTag = tbx.Tag.ToString().Split('_');
-                    Sizes size = arrTbxTag[0] == Sizes.kleine.ToString() ? Sizes.kleine :
-                                 arrTbxTag[0] == Sizes.middelgrote.ToString() ? Sizes.middelgrote : Sizes.grote;
-                    Types type = arrTbxTag[1] == Types.planteter.ToString() ? Types.planteter : Types.vleeseter;
-                    int weight = Convert.ToInt32(size);
+                string[] arrTbxTag = tbx.Tag.ToString().Split('_');
+                Sizes size = arrTbxTag[0] == Sizes.kleine.ToString() ? Sizes.kleine :
+                                arrTbxTag[0] == Sizes.middelgrote.ToString() ? Sizes.middelgrote : Sizes.grote;
+                Types type = arrTbxTag[1] == Types.planteter.ToString() ? Types.planteter : Types.vleeseter;
+                int weight = Convert.ToInt32(size);
 
-                    for (int i = 0; i < Convert.ToInt32(tbx.Text); i++)
-                        animals.Add(new Animal(size, type, weight));
-                }
+                for (int i = 0; i < Convert.ToInt32(tbx.Text); i++)
+                    animals.Add(new Animal(size, type, weight));
             }
             return animals;
         }
@@ -47,8 +44,8 @@ namespace Circustrein
         /// <summary>
         /// Return all controls named T inside depObj
         /// </summary>
-        /// <typeparam name="T">Name of the wanted control</typeparam>
-        /// <param name="depObj">Name of the object in which the wanted controls are</param>
+        /// <typeparam name="T">Wanted control</typeparam>
+        /// <param name="depObj">Object in which the wanted controls are</param>
         private static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
         {
             if (depObj != null)
