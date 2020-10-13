15ba2c12216e6ca0691fcbc808801dff3f7e7414
Robbert Lengton <robbert.lengton@hotmail.nl>
2020-10-13 13:55:05 +0200

    Succesvolle unit test geschreven voor methode EnoughSpace in WagonTests.

--- CircustreinTests/WagonTests.cs
+++ CircustreinTests/WagonTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Circustrein.Tests
 {
@@ -8,7 +9,27 @@ namespace Circustrein.Tests
         [TestMethod()]
         public void EnoughSpaceTest()
         {
-            Assert.AreEqual(true,true);
+            // Arrange
+            bool expected = false;
+
+            // Act
+            List<Animal> animals = new List<Animal>
+            {
+                new Animal(Sizes.grote, Types.planteter),
+                new Animal(Sizes.middelgrote, Types.planteter)
+            };
+
+            int wagonID = 1;
+            Wagon wagon = new Wagon(wagonID);
+            wagon.AddAnimalToWagon(animals, animals[0]);
+            wagon.AddAnimalToWagon(animals, animals[0]);
+
+            Animal animalThatWantsToEnter = new Animal(Sizes.middelgrote, Types.planteter);
+
+            bool actual = wagon.EnoughSpace(animalThatWantsToEnter);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
